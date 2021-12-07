7093b0ff2b6008911b93ae108780894c380ffcf3
JY2452 <jaeyoung.pak8@gmail.com>
2021-12-07 00:37:08 -0500

    profile styling

--- src/components/profile/profile.js
+++ src/components/profile/profile.js
@@ -6,20 +6,56 @@ import { signoutUser } from '../../actions';
 const Profile = (props) => {
   return (
     <section id="profile">
-      <button type="button" onClick={() => props.signoutUser(props.history)}>Sign Out</button>
       {props.user ? (
         <div>
-          <img src={props.user.picture} alt="profile" />
-          <p>{props.user.name}</p>
-          <p>{props.user.gender}</p>
-          <p>{props.user.birthday}</p>
-          <p>{props.user.home}</p>
-          <p>{props.user.quote}</p>
-          <p>{props.user.favoriteShoe}</p>
-          <p>{props.user.favoriteArtist}</p>
-          <p>{props.user.favoriteColor}</p>
+
+          <div id="userInfo">
+            <img className="profilePic" src={props.user.picture} alt="profile" />
+
+            <div id="userText">
+              <div id="titleRow">
+                <h2>{props.user.username}</h2>
+                <button type="button" onClick={() => console.log('hi')}>Edit Profile</button>
+                <button type="button" onClick={() => props.signoutUser(props.history)}>Log Out</button>
+              </div>
+
+              <div id="doodleRow">
+                <h3>{props.user.name}</h3>
+                <p>🖼️ <b>100</b> Doodles 🖼️</p>
+              </div>
+
+              <div className="textRow">
+                <p style={{ backgroundColor: '#f8961e' }}>👤 {props.user.gender}</p>
+                <p style={{ backgroundColor: '#43aa8b', color: 'white' }}>🎨 {props.user.favoriteColor}</p>
+                <p style={{ backgroundColor: '#f9c74f' }}>📍 {props.user.home}</p>
+              </div>
+
+              <div className="textRow">
+                <p style={{ backgroundColor: '#f94144', color: 'white' }}>🎂 {props.user.birthday}</p>
+                <p style={{ backgroundColor: '#90be6d' }}>👟 {props.user.favoriteShoe}</p>
+                <p style={{ backgroundColor: '#277da1', color: 'white' }}>🎧 {props.user.favoriteArtist}</p>
+              </div>
+
+              <div className="textRow">
+                <p>{props.user.quote}</p>
+              </div>
+            </div>
+          </div>
+
+          <div id="userDoodles">
+            <h2>Doodles</h2>
+            <div className="doodleGallery">
+              <div className="doodle" />
+              <div className="doodle" />
+              <div className="doodle" />
+              <div className="doodle" />
+              <div className="doodle" />
+              <div className="doodle" />
+              <div className="doodle" />
+            </div>
+          </div>
         </div>
-      ) : <p>Hi</p> }
+      ) : null }
     </section>
   );
 };
