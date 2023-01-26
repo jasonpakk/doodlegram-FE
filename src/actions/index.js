6635fdd8720a1eb8c92f591be8eae037fdda3997
Jason Pak <55042077+JY2452@users.noreply.github.com>
2023-01-25 23:00:40 -0500

    Update index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const ROOT_URL = 'https://pak-doodlegram.herokuapp.com/api';
+export const ROOT_URL = 'https://doodlegram-be.onrender.com/api';
 // export const ROOT_URL = 'http://localhost:9090/api';
 
 // keys for actiontypes
